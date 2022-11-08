5f732fa0019e6a370c0a5f8ffa14ae30aadc8824
Carlos <correamillancarlos@gmail.com>
2022-11-08 13:35:12 -0400

    workbox added

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	globDirectory: "dist/",
-	globPatterns: ["**/*.{css,js,html,webmanifest,svg}"],
+	globPatterns: ["**/*.{css,js,html,webmanifest,svg,json}"],
 	swDest: "dist/sw.js",
 	swSrc: "src/sw-template.js",
 	// ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
